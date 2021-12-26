04ce6149a1c219c45a70c6798fae9c5c67f72944
Kittipat <first1801@hotmail.com>
2021-12-26 13:38:26 +0700

    Minimap Show PlayernameHideInShadow Skip Btn Function

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    private int[] IntArray;
+    private List<int> ListInt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ListInt.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
