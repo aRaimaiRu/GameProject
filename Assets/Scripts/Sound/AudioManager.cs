6e593c4477178e3c6faf9683465a6076db32719a
Kittipat <first1801@hotmail.com>
2022-03-31 21:15:01 +0700

    adjust sound

--- Assets/Scripts/Sound/AudioManager.cs
+++ Assets/Scripts/Sound/AudioManager.cs
@@ -2,11 +2,21 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
+    [SerializeField] Slider bgmSlider;
+    [SerializeField] Slider sfxSlider;
+    [SerializeField] Image BG;
+    [SerializeField] AudioSource bgm;
+    [SerializeField] AudioSource sfx;
+
+
     public Sound[] sounds;
+
     public static AudioManager instance;
+
 
     private void Awake()
     {
@@ -15,12 +25,22 @@ public class AudioManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
         foreach (Sound s in sounds)
         {
-            s.source = gameObject.AddComponent<AudioSource>();
+            // s.source = gameObject.AddComponent<AudioSource>();
             s.source.clip = s.clip;
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
         }
+    }
+    private void Start()
+    {
+        bgmSlider.onValueChanged.AddListener((float value) => setVolume("bgm", value));
+        sfxSlider.onValueChanged.AddListener((float value) => setVolume("sfx", value));
+
+
+        bgmSlider.value = bgm.volume;
+        sfxSlider.value = sfx.volume;
+
     }
 
     public void Play(string name)
@@ -29,6 +49,24 @@ public class AudioManager : MonoBehaviour
         if (s == null) return;
         s.source.Play();
     }
+
+    public void setVolume(string volumeType, float value)
+    {
+        Sound[] mysounds = Array.FindAll<Sound>(sounds, sound => sound.source.gameObject.name == volumeType);
+        foreach (Sound s in mysounds)
+        {
+            s.volume = value;
+            Debug.Log(s.source.gameObject.name);
+            s.source.volume = value;
+
+
+        }
+    }
+    public void setCanvasActive(bool _active)
+    {
+        BG.gameObject.SetActive(_active);
+    }
+
 
 
 }
